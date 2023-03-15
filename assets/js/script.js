9a84a57be3fdcd3a021a47857b01a94c2b1a37dc
Aidin <devxwc@gmail.com>
2023-03-15 09:34:48 +0600

    fixed pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,14 @@ hamburger.addEventListener('click', e => {
 
   toggleMenu();
 });
+
+document.addEventListener('click', e => {
+  let target = e.target;
+  let its_menu = target == menu || menu.contains(target);
+  let its_hamburger = target == hamburger;
+  let menu_is_active = menu.classList.contains('is-active');
+  
+  if (!its_menu && !its_hamburger && menu_is_active) {
+    toggleMenu();
+  }
+});
